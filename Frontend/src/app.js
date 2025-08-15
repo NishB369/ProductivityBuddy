ccb83868024b6b29ba4671b83576e872cacf6ede
nishb <nishbcodes@gmail.com>
2025-08-15 19:51:37 +0530

    Landing Page Initiated

--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom/client";
 import Workspace from "./Components/Workspaces/Workspace";
 import WorkspaceDetail from "./Components/Workspaces/WorkspaceDetail";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
+import LandingPage from "./Pages/LandingPage";
 
 const App = () => {
   return (
     <Router>
       <div className="h-[100%] w-[100%] bg-[#191919]">
         <Routes>
-          <Route path="/" element={<Workspace />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/workspace" element={<Workspace />} />
           <Route path="/workspace/:id" element={<WorkspaceDetail />} />
         </Routes>
       </div>
